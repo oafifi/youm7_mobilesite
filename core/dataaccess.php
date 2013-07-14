6acfd6c06bf5301ccd050aacf5eeb423bf280152
Omar Afifi <omr_a@hotmail.com>
2013-07-14 20:47:09 +0200

    test
    test

--- core/dataaccess.php
+++ core/dataaccess.php
@@ -1,220 +1,195 @@
-
-
 <?php
-
-	require_once "core/datastructures.php";
-	
-	interface NewsDao {
-		
-		function getSectionNews($secId,$page = 1);
-		function getNewsContent($newsId);
+require_once "core/datastructures.php";
+interface NewsDao {
+	function getSectionNews($secId, $page = 1);
+	function getNewsContent($newsId);
+}
+class DaoFactory {
+	const NEWS_SERVICE_DAO = 1;
+	static function getDao($type) {
+		switch ($type) {
+			case self::NEWS_SERVICE_DAO :
+				return new NewsServiceDao ();
+		}
 	}
-	
-	class DaoFactory{
-		const NEWS_SERVICE_DAO = 1;
+}
+class NewsServiceDao implements NewsDao {
+	private $tempList; // holds list of values and objects during parsing
+	private $currentTag; // holds current tag
+	private $tempNewsAbs; // holds temp NewsAbstract objects during parsing
+	private $tempNewsCon; // holds temp NewsContent objects during parsing
+	private $tempNews; // holds temp News objects during parsing
+	private $rNewsFlag; // flag to indicate parsing related news
+	private function doGetRequest($url) {
+		$ch = curl_init ( $url );
+		curl_setopt ( $ch, CURLOPT_RETURNTRANSFER, true );
+		$response = curl_exec ( $ch );
+		curl_close ( $ch );
+		return $response;
+	}
+	public function getSectionNews($secId, $page = 1) {
+		$url = "http://mobrss.youm7.com/rss/service.svc/SelectForSpecifiedSection/SecID/$secId/page/$page";
+		$response = $this->doGetRequest ( $url );
+		$parser = xml_parser_create ( "UTF-8" );
 		
-		static function getDao($type){
-			switch($type){
-				case self::NEWS_SERVICE_DAO:
-					return new NewsServiceDao();
+		xml_set_element_handler ( 		// anonymous handler functions used here
+		$parser, function ($parser, $name, $attributes) { // start tag handler
+			$name = strtolower ( $name );
+			switch ($name) {
+				case 'feeds' :
+					$this->tempList = array ();
+					break;
+				case 'item' :
+					$this->tempNewsAbs = new NewsAbstract ();
+					break;
+				default :
+					$this->currentTag = $name;
+					break;
 			}
-		}
+		}, function ($parser, $name) { // end tag handler
+			$name = strtolower ( $name );
+			if ($name == 'item')
+				$this->tempList [] = $this->tempNewsAbs;
+		} );
 		
+		xml_set_character_data_handler ( 		// anonymous handler functions used here
+		$parser, function ($parser, $data) { // character data handler
+			switch ($this->currentTag) {
+				case 'title' :
+					$this->tempNewsAbs->setTitle ( $this->tempNewsAbs->getTitle () . $data );
+					break;
+				case 'id' :
+					$this->tempNewsAbs->setId ( $this->tempNewsAbs->getId () . $data );
+					break;
+				case 'image' :
+					$this->tempNewsAbs->setImage ( $this->tempNewsAbs->getImage () . $data );
+					break;
+				case 'pubdate' :
+					$this->tempNewsAbs->setPubDate ( $this->tempNewsAbs->getPubDate () . $data );
+					break;
+				case 'date' :
+					$this->tempNewsAbs->setDate ( $this->tempNewsAbs->getDate () . $data );
+					break;
+				case 'mainimage' :
+					$this->tempNewsAbs->setMainImage ( $this->tempNewsAbs->getMainImage () . $data );
+					break;
+				case 'abstract' :
+					$this->tempNewsAbs->setNewsAbstract ( $this->tempNewsAbs->getNewsAbstract () . $data );
+					break;
+				case 'rownum' :
+					$this->tempNewsAbs->setRowNum ( $this->tempNewsAbs->getRowNum () . $data );
+					break;
+				case 'imagesonly' :
+					$this->tempNewsAbs->setImagesOnly ( $this->tempNewsAbs->getImagesOnly () . $data );
+					break;
+			}
+		} );
+		
+		xml_parse ( $parser, $response );
+		xml_parser_free ( $parser );
+		
+		return $this->tempList;
 	}
-	
-    class NewsServiceDao implements NewsDao{
-        
-		private $tempList;    //holds list of values and objects during parsing
-        private $currentTag;    //holds current tag
-        private $tempNewsAbs;   //holds temp NewsAbstract objects during parsing
-		private $tempNewsCon;   //holds temp NewsContent objects during parsing
-		private $tempNews;	//holds temp News objects during parsing
-		private $rNewsFlag;	//flag to indicate parsing related news
+	public function getNewsContent($newsId) {
+		$url = "http://mobrss.youm7.com/rss/service.svc/SelectForNewsDetails/id/$newsId";
+		$response = $this->doGetRequest ( $url );
+		$parser = xml_parser_create ( "UTF-8" );
+		$this->rNewsFlag = false;
 		
-        private function doGetRequest($url){
-            $ch = curl_init($url);
-            curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
-            $response = curl_exec($ch);
-            curl_close($ch);
-            return $response;
-        }
-
-        public function getSectionNews($secId,$page = 1){
-            
-            $url="http://mobrss.youm7.com/rss/service.svc/SelectForSpecifiedSection/SecID/$secId/page/$page";
-            $response = $this->doGetRequest($url);
-            $parser = xml_parser_create("UTF-8");
-            
-            xml_set_element_handler(    //anonymous handler functions used here
-                                    $parser,
-                                    function($parser, $name, $attributes){   //start tag handler
-                                        $name=strtolower($name);
-                                        switch($name){
-                                            case 'feeds':
-                                                $this->tempList = array();
-                                                break;
-                                            case 'item':
-                                                $this->tempNewsAbs=new NewsAbstract();
-                                                break;
-                                            default:
-                                                $this->currentTag=$name;
-                                                break;
-                                        }
-                                    },
-                                    function($parser, $name){  //end tag handler
-                                        $name=strtolower($name);
-                                        if($name == 'item')
-                                            $this->tempList[]=$this->tempNewsAbs;
-                                    }
-                                    );
-            xml_set_character_data_handler(    //anonymous handler functions used here
-                                           $parser,
-                                           function($parser,$data){  //character data handler
-                                                switch($this->currentTag){
-                                                    case 'title':
-                                                        $this->tempNewsAbs->setTitle($this->tempNewsAbs->getTitle().$data);
-                                                        break;
-                                                    case 'id':
-                                                        $this->tempNewsAbs->setId($this->tempNewsAbs->getId().$data);
-                                                        break;
-                                                    case 'image':
-                                                        $this->tempNewsAbs->setImage($this->tempNewsAbs->getImage().$data);
-                                                        break;
-                                                    case 'pubdate':
-                                                        $this->tempNewsAbs->setPubDate($this->tempNewsAbs->getPubDate().$data);
-                                                        break;
-                                                    case 'date':
-                                                        $this->tempNewsAbs->setDate($this->tempNewsAbs->getDate().$data);
-                                                        break;
-                                                    case 'mainimage':
-                                                        $this->tempNewsAbs->setMainImage($this->tempNewsAbs->getMainImage().$data);
-                                                        break;
-                                                    case 'abstract':
-                                                        $this->tempNewsAbs->setNewsAbstract($this->tempNewsAbs->getNewsAbstract().$data);
-                                                        break;
-                                                    case 'rownum':
-                                                        $this->tempNewsAbs->setRowNum($this->tempNewsAbs->getRowNum().$data);
-                                                        break;
-                                                    case 'imagesonly':
-                                                        $this->tempNewsAbs->setImagesOnly($this->tempNewsAbs->getImagesOnly().$data);
-                                                        break;
-                                                }
-                                           }
-                                           );
-            xml_parse($parser,$response);
-            xml_parser_free($parser);
-            
-            return $this->tempList;
-        }
+		xml_set_element_handler ( 		// anonymous handler functions used here
+		$parser, function ($parser, $name, $attributes) { // start tag handler
+			$name = strtolower ( $name );
+			switch ($name) {
+				case 'item' :
+					$this->tempNewsCon = new NewsContent ();
+					break;
+				case 'images' :
+					$this->tempList = array ();
+					break;
+				case 'videos' :
+					$this->tempList = array ();
+					break;
+				case 'relatednews' :
+					$this->tempList = array ();
+					$this->rNewsFlag = true;
+					break;
+				case 'news' :
+					$this->tempNews = new News ();
+					break;
+				default :
+					$this->currentTag = $name;
+					break;
+			}
+		}, function ($parser, $name) { // end tag handler
+			$name = strtolower ( $name );
+			switch ($name) {
+				case 'images' :
+					$this->tempNewsCon->setImageList ( $this->tempList );
+					$this->tempList = null;
+					break;
+				case 'videos' :
+					$this->tempNewsCon->setVideoList ( $this->tempList );
+					$this->tempList = null;
+					break;
+				case 'news' :
+					$this->tempList [] = $this->tempNews;
+					break;
+				case 'relatednews' :
+					$this->tempNewsCon->setRelNewsList ( $this->tempList );
+					$this->rNewsFlag = false;
+					break;
+			}
+		} );
 		
+		xml_set_character_data_handler ( 		// anonymous handler functions used here
+		$parser, function ($parser, $data) { // character data handler
+			switch ($this->currentTag) {
+				case 'pubdate' :
+					$this->tempNewsCon->setPubDate ( $this->tempNewsCon->getPubDate () . $data );
+					break;
+				case 'date' :
+					$this->tempNewsCon->setDate ( $this->tempNewsCon->getDate () . $data );
+					break;
+				case 'mainimage' :
+					$this->tempNewsCon->setMainImage ( $this->tempNewsCon->getMainImage () . $data );
+					break;
+				case 'link' :
+					$this->tempNewsCon->setLink ( $this->tempNewsCon->getLink () . $data );
+					break;
+				case 'title' :
+					if (! $this->rNewsFlag) {
+						$this->tempNewsCon->setTitle ( $this->tempNewsCon->getTitle () . $data );
+					} else {
+						$this->tempNews->setTitle ( $this->tempNews->getTitle () . $data );
+					}
+					break;
+				case 'body' :
+					$this->tempNewsCon->setBody ( $this->tempNewsCon->getBody () . $data );
+					break;
+				case 'id' :
+					if (! $this->rNewsFlag) {
+						$this->tempNewsCon->setId ( $this->tempNewsCon->getId () . $data );
+					} else {
+						$this->tempNews->setId ( $this->tempNews->getId () . $data );
+					}
+					break;
+				case 'numberofcomments' :
+					$this->tempNewsCon->setCommentsNo ( $this->tempNewsCon->getCommentsNo () . $data );
+					break;
+				case 'image' :
+					$this->tempList [] = $data;
+					break;
+				case 'video' :
+					$this->tempList [] = $data;
+					break;
+			}
+		} );
 		
-		public function getNewsContent($newsId){
-			$url="http://mobrss.youm7.com/rss/service.svc/SelectForNewsDetails/id/$newsId";
-            $response = $this->doGetRequest($url);
-            $parser = xml_parser_create("UTF-8");
-			$this->rNewsFlag=false;
-			
-			xml_set_element_handler(    //anonymous handler functions used here
-                                    $parser,
-                                    function($parser, $name, $attributes){   //start tag handler
-                                        $name=strtolower($name);
-                                        switch($name){
-                                            case 'item':
-                                                $this->tempNewsCon=new NewsContent();
-                                                break;
-											case 'images':
-                                                $this->tempList=array();
-                                                break;
-											case 'videos':
-                                                $this->tempList=array();
-                                                break;
-											case 'relatednews':
-                                                $this->tempList=array();
-												$this->rNewsFlag = true;
-                                                break;
-											case 'news':
-                                                $this->tempNews=new News();
-                                                break;
-                                            default:
-                                                $this->currentTag=$name;
-                                                break;
-                                        }
-                                    },
-                                    function($parser, $name){  //end tag handler
-                                        $name=strtolower($name);
-                                        switch($name){
-											case 'images':
-												$this->tempNewsCon->setImageList($this->tempList);
-												$this->tempList=null;
-												break;
-											case 'videos':
-												$this->tempNewsCon->setVideoList($this->tempList);
-												$this->tempList=null;
-												break;
-											case 'news':
-												$this->tempList[]=$this->tempNews;
-												break;
-											case 'relatednews':
-												$this->tempNewsCon->setRelNewsList($this->tempList);
-												$this->rNewsFlag=false;
-												break;
-										}
-                                    }
-                                    );
-			xml_set_character_data_handler(    //anonymous handler functions used here
-                                           $parser,
-                                           function($parser,$data){  //character data handler
-                                                switch($this->currentTag){
-													case 'pubdate':
-                                                        $this->tempNewsCon->setPubDate($this->tempNewsCon->getPubDate().$data);
-                                                        break;
-													case 'date':
-                                                        $this->tempNewsCon->setDate($this->tempNewsCon->getDate().$data);
-                                                        break;
-                                                    case 'mainimage':
-                                                        $this->tempNewsCon->setMainImage($this->tempNewsCon->getMainImage().$data);
-                                                        break;
-                                                    case 'link':
-                                                        $this->tempNewsCon->setLink($this->tempNewsCon->getLink().$data);
-                                                        break;
-                                                    case 'title':
-														if(!$this->rNewsFlag){
-															$this->tempNewsCon->setTitle($this->tempNewsCon->getTitle().$data);
-														}
-														else{
-															$this->tempNews->setTitle($this->tempNews->getTitle().$data);
-														}
-                                                        break;
-													case 'body':
-                                                        $this->tempNewsCon->setBody($this->tempNewsCon->getBody().$data);
-                                                        break;
-                                                    case 'id':
-														if(!$this->rNewsFlag){
-															$this->tempNewsCon->setId($this->tempNewsCon->getId().$data);
-														}
-														else{
-															$this->tempNews->setId($this->tempNews->getId().$data);
-														}
-                                                        break;
-													case 'numberofcomments':
-                                                        $this->tempNewsCon->setCommentsNo($this->tempNewsCon->getCommentsNo().$data);
-                                                        break;
-                                                    case 'image':
-                                                        $this->tempList[]=$data;
-                                                        break;
-													case 'video':
-                                                        $this->tempList[]=$data;
-                                                        break;
-                                                }
-                                           }
-                                           );
-			xml_parse($parser,$response);
-            xml_parser_free($parser);
-			
-			return $this->tempNewsCon;
-		}
-
-    }
-	
+		xml_parse ( $parser, $response );
+		xml_parser_free ( $parser );
+		
+		return $this->tempNewsCon;
+	}
+}
 
 ?>
--- core/datastructures.php
+++ core/datastructures.php
@@ -1,120 +1,121 @@
 <?php
-class News{
 
-    protected $title;
-    protected $id;
-    protected $pubDate;
-    protected $date;
-    protected $mainImage;
-    
-    public function getTitle(){
-        return $this->title;
-    }
-    public function setTitle($t){
-        $this->title = $t;
-    }
-    public function getId(){
-        return $this->id;
-    }
-    public function setId($newId){
-        $this->id = $newId;
-    }
-    public function getPubDate(){
-        return $this->pubDate;
-    }
-    public function setPubDate($newPubDate){
-        $this->pubDate = $newPubDate;
-    }
-    public function getDate(){
-        return $this->date;
-    }
-    public function setDate($newDate){
-        $this->date = $newDate;
-    }
-    public function getMainImage(){
-        return $this->mainImage;
-    }
-    public function setMainImage($newMainImage){
-        $this->mainImage = $newMainImage;
-    }
-
+class News {
+	
+	protected $title;
+	protected $id;
+	protected $pubDate;
+	protected $date;
+	protected $mainImage;
+	
+	public function getTitle() {
+		return $this->title;
+	}
+	public function setTitle($t) {
+		$this->title = $t;
+	}
+	public function getId() {
+		return $this->id;
+	}
+	public function setId($newId) {
+		$this->id = $newId;
+	}
+	public function getPubDate() {
+		return $this->pubDate;
+	}
+	public function setPubDate($newPubDate) {
+		$this->pubDate = $newPubDate;
+	}
+	public function getDate() {
+		return $this->date;
+	}
+	public function setDate($newDate) {
+		$this->date = $newDate;
+	}
+	public function getMainImage() {
+		return $this->mainImage;
+	}
+	public function setMainImage($newMainImage) {
+		$this->mainImage = $newMainImage;
+	}
 }
-
-class NewsAbstract extends News{
-    protected $image;
-    protected $newsAbstract;
-    protected $rowNum;
-    protected $imagesOnly;
-    
-    public function getImage(){
-        return $this->image;
-    }
-    public function setImage($i){
-        $this->image = $i;
-    }
-    public function getNewsAbstract(){
-        return $this->newsAbstract;
-    }
-    public function setNewsAbstract($a){
-        $this->newsAbstract = $a;
-    }
-    public function getRowNum(){
-        return $this->rowNum;
-    }
-    public function setRowNum($n){
-        $this->rowNum = $n;
-    }
-    public function getImagesOnly(){
-        return $this->imagesOnly;
-    }
-    public function setImagesOnly($o){
-        $this->imagesOnly = $o;
-    }
+class NewsAbstract extends News {
+	
+	protected $image;
+	protected $newsAbstract;
+	protected $rowNum;
+	protected $imagesOnly;
+	
+	public function getImage() {
+		return $this->image;
+	}
+	public function setImage($i) {
+		$this->image = $i;
+	}
+	public function getNewsAbstract() {
+		return $this->newsAbstract;
+	}
+	public function setNewsAbstract($a) {
+		$this->newsAbstract = $a;
+	}
+	public function getRowNum() {
+		return $this->rowNum;
+	}
+	public function setRowNum($n) {
+		$this->rowNum = $n;
+	}
+	public function getImagesOnly() {
+		return $this->imagesOnly;
+	}
+	public function setImagesOnly($o) {
+		$this->imagesOnly = $o;
+	}
 }
-
 class NewsContent extends News {
-    protected $link;
-    protected $body;
-    protected $commentsNo;
-    protected $imageList;   //array of images links
-    protected $videoList;   //array of video links
-    protected $relNewsList; //array of related news stored in News objects
-    
-    public function getLink(){
-        return $this->link;
-    }
-    public function setLink($l){
-        $this->link = $l;
-    }
-    public function getBody(){
-        return $this->body;
-    }
-    public function setBody($b){
-        $this->body = $b;
-    }
-    public function getCommentsNo(){
-        return $this->commentsNo;
-    }
-    public function setCommentsNo($n){
-        $this->commentsNo = $n;
-    }
-    public function getImageList(){
-        return $this->imageList;
-    }
-    public function setImageList($list){
-        $this->imageList = $list;
-    }
-    public function getVideoList(){
-        return $this->videoList;
-    }
-    public function setVideoList($list){
-        $this->videoList = $list;
-    }
-    public function getRelNewsList(){
-        return $this->relNewsList;
-    }
-    public function setRelNewsList($list){
-        $this->relNewsList = $list;
-    }
+	
+	protected $link;
+	protected $body;
+	protected $commentsNo;
+	protected $imageList; // array of images links
+	protected $videoList; // array of video links
+	protected $relNewsList; // array of related news stored in News objects
+	
+	public function getLink() {
+		return $this->link;
+	}
+	public function setLink($l) {
+		$this->link = $l;
+	}
+	public function getBody() {
+		return $this->body;
+	}
+	public function setBody($b) {
+		$this->body = $b;
+	}
+	public function getCommentsNo() {
+		return $this->commentsNo;
+	}
+	public function setCommentsNo($n) {
+		$this->commentsNo = $n;
+	}
+	public function getImageList() {
+		return $this->imageList;
+	}
+	public function setImageList($list) {
+		$this->imageList = $list;
+	}
+	public function getVideoList() {
+		return $this->videoList;
+	}
+	public function setVideoList($list) {
+		$this->videoList = $list;
+	}
+	public function getRelNewsList() {
+		return $this->relNewsList;
+	}
+	public function setRelNewsList($list) {
+		$this->relNewsList = $list;
+	}
 }
+
 ?>
